c98e23c099449b51ebdf10ecca7332c88535587d
Noskovsk <dicksfriend@yandex.ru>
2022-11-20 22:16:08 +0300

    feat: add some tests

--- src/test/java/ru/practicum/shareit/service/ItemServiceImplMockTest.java
+++ src/test/java/ru/practicum/shareit/service/ItemServiceImplMockTest.java
@@ -1,10 +1,14 @@
 package ru.practicum.shareit.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.web.server.ResponseStatusException;
+import ru.practicum.shareit.booking.BookingStatus;
 import ru.practicum.shareit.booking.dao.BookingRepository;
+import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.comment.dao.CommentRepository;
+import ru.practicum.shareit.item.comment.dto.CommentDto;
 import ru.practicum.shareit.item.comment.model.Comment;
 import ru.practicum.shareit.item.dao.ItemRepository;
 import ru.practicum.shareit.item.model.Item;
@@ -16,36 +20,56 @@ import ru.practicum.shareit.user.service.UserServiceImpl;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 public class ItemServiceImplMockTest {
+    Comment comment;
+    User user;
+    Item item;
+    UserService userMockService = Mockito.mock(UserServiceImpl.class);
+    ItemRepository itemMockRepository = Mockito.mock(ItemRepository.class);
+    BookingRepository bookingMockRepository = Mockito.mock(BookingRepository.class);
+    CommentRepository commentMockRepository = Mockito.mock(CommentRepository.class);
+    ItemService itemService = new ItemServiceImpl(itemMockRepository, bookingMockRepository, userMockService, commentMockRepository);
+
+    @BeforeEach
+    void createTestData() {
+        comment = new Comment();
+        comment.setText("some text");
+
+        user = new User();
+        user.setEmail("email@email.com");
+
+        item = new Item();
+        item.setOwner(user);
+    }
 
     @Test
     void shouldThrowExceptionWhenAddCommentByIncorrectUser() {
-        Comment comment = new Comment();
-        comment.setText("some text");
-
-        User user = new User();
-        user.setEmail("email@email.com");
-
-        Item item = new Item();
-        item.setOwner(user);
-
-        UserService userMockService = Mockito.mock(UserServiceImpl.class);
-        ItemRepository itemMockRepository = Mockito.mock(ItemRepository.class);
-        BookingRepository bookingMockRepository = Mockito.mock(BookingRepository.class);
-        CommentRepository commentMockRepository = Mockito.mock(CommentRepository.class);
-        ItemService itemService = new ItemServiceImpl(itemMockRepository, bookingMockRepository, userMockService, commentMockRepository);
 
         when(userMockService.getUserById(1L)).thenReturn(user);
-
         when(itemMockRepository.findById(any())).thenReturn(Optional.of(item));
 
         Throwable throwable = assertThrows(ResponseStatusException.class,
                 () -> itemService.addComment(1L, 1L, comment));
         assertTrue(throwable.getMessage().contains("400"));
+    }
+
+    @Test
+    void shouldAddComment() {
+        when(userMockService.getUserById(1L)).thenReturn(user);
+        when(itemMockRepository.findById(any())).thenReturn(Optional.of(item));
+        when(bookingMockRepository
+                .getBookingByBookerAndItemAndStatusEqualsAndEndBefore(any(), any(), eq(BookingStatus.APPROVED), any()))
+                .thenReturn(Optional.of(new Booking()));
+        when(commentMockRepository.save(any())).thenReturn(comment);
+        comment.setId(1L);
+        CommentDto commentDto = itemService.addComment(1L, 1L, comment);
+
+        assertEquals("some text", commentDto.getText(), "Текст комментария не совпадает.");
+        assertEquals(1L, commentDto.getId(), "Не совпадает идентефикатор комментария.");
     }
 }
