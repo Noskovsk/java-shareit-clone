7fc9e7092fb3a66ba975a6f385acc001a7eee321
Noskovsk <dicksfriend@yandex.ru>
2022-11-22 23:30:41 +0300

    feat: add some tests

--- src/test/java/ru/practicum/shareit/service/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/service/BookingServiceImplTest.java
@@ -4,13 +4,21 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.booking.BookingStatus;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.service.ItemService;
 import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Transactional
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -18,6 +26,7 @@ import java.util.List;
         webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class BookingServiceImplTest {
     private final BookingService bookingService;
+    private final ItemService itemService;
     private final EntityManager manager;
 
     protected List<User> createTestUserIntoDb(Integer count) {
@@ -32,9 +41,38 @@ public class BookingServiceImplTest {
         return userList;
     }
 
+    protected Item createItem(User user, boolean available) {
+        Item item = new Item();
+        item.setName("itemName");
+        item.setDescription("itemDescription");
+        item.setAvailable(available);
+        item.setOwner(user);
+        manager.persist(item);
+        manager.flush();
+        return item;
+    }
+
 
     @Test
     void shouldCreateBooking() {
+        List<User> userList = createTestUserIntoDb(2);
+        User userOwner = userList.get(0);
+        User userBooker = userList.get(1);
+        Item item = createItem(userOwner, true);
+        LocalDateTime localDateTimeStart = LocalDateTime.now().plusDays(1);
+        LocalDateTime localDateTimeEnd = LocalDateTime.now().plusDays(2);
+        BookingDto bookingDto = BookingDto
+                .builder()
+                .itemId(item.getId())
+                .start(localDateTimeStart)
+                .end(localDateTimeEnd)
+                .bookerId(userBooker.getId()).build();
+        Booking booking = bookingService.createBooking(userBooker.getId(), bookingDto);
 
+        assertEquals(localDateTimeStart, booking.getStart(),
+                "Время начала бронирования не совпадает");
+        assertEquals(localDateTimeEnd, booking.getEnd(),
+                "Время окончания бронирования не совпадает");
+        assertEquals(BookingStatus.WAITING, booking.getStatus(), "Не совпадает статус у бронирования");
     }
 }
